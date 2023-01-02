3962378d4838bcadcf1f50b3473e65cf84b250d3
rightson <chari.rightson@gmail.com>
2023-01-02 23:41:37 +0300

    5

--- pages/api/adminlist/index.js
+++ pages/api/adminlist/index.js
@@ -1,55 +1,55 @@
-import db from "../../../utils/admin";
-import NextCors from "nextjs-cors";
-import handleCors from "../../../handleCors";
+// import db from "../../../utils/admin";
+// import NextCors from "nextjs-cors";
+// import handleCors from "../../../handleCors";
 
-const handler = async(req, res) => {
-    const { method } = req;
-    handleCors(req, res);
+// const handler = async(req, res) => {
+//     const { method } = req;
+//     handleCors(req, res);
 
-    if (method === "GET") {
-        const admins = await db.collection("admins").get();
-        const admin = admins.docs.map((doc) => doc.data());
+//     if (method === "GET") {
+//         const admins = await db.collection("admins").get();
+//         const admin = admins.docs.map((doc) => doc.data());
 
-        res.status(200).json(admin);
-    } else if (method === "POST") {
-        try {
-            const { id } = await db.collection("admins").add({
-                ...req.body,
-                created: new Date().toISOString(),
-            });
-            res.status(200).json({ id });
-        } catch (e) {
-            res.status(400).json({ message: "Something went wrong" });
-        }
-    } else if (method === "DELETE") {
-        const { id } = req.query;
-        try {
-            const admin = db.collection("admins").where("id", "==", id);
-            admin.get().then((querySnapshot) => {
-                querySnapshot.forEach(function(doc) {
-                    doc.ref.delete();
-                });
-            });
-            res.status(200).json("deleted");
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    } else if (method === "PUT") {
-        const { id } = req.query;
-        try {
-            const admin = await db
-                .collection("admins")
-                .doc(id)
-                .update({
-                    ...req.body,
-                    updated: new Date().toISOString(),
-                });
-            res.status(200).json(admin);
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    } else {
-        res.status(200).json({ error: "Method not allowed" });
-    }
-};
-export default handler;
+//         res.status(200).json(admin);
+//     } else if (method === "POST") {
+//         try {
+//             const { id } = await db.collection("admins").add({
+//                 ...req.body,
+//                 created: new Date().toISOString(),
+//             });
+//             res.status(200).json({ id });
+//         } catch (e) {
+//             res.status(400).json({ message: "Something went wrong" });
+//         }
+//     } else if (method === "DELETE") {
+//         const { id } = req.query;
+//         try {
+//             const admin = db.collection("admins").where("id", "==", id);
+//             admin.get().then((querySnapshot) => {
+//                 querySnapshot.forEach(function(doc) {
+//                     doc.ref.delete();
+//                 });
+//             });
+//             res.status(200).json("deleted");
+//         } catch (error) {
+//             res.status(500).json(error);
+//         }
+//     } else if (method === "PUT") {
+//         const { id } = req.query;
+//         try {
+//             const admin = await db
+//                 .collection("admins")
+//                 .doc(id)
+//                 .update({
+//                     ...req.body,
+//                     updated: new Date().toISOString(),
+//                 });
+//             res.status(200).json(admin);
+//         } catch (error) {
+//             res.status(500).json(error);
+//         }
+//     } else {
+//         res.status(200).json({ error: "Method not allowed" });
+//     }
+// };
+// export default handler;
